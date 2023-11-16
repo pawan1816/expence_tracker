54d9f827a4a0d42d8c28ad9062a93def4bdf74c0
pawan kumar paik <paikpawan18@gmail.com>
2023-11-16 23:21:20 +0530

    ..

--- expence_tracker/src/App.js
+++ expence_tracker/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Expence from "./component/Expence";
 
-function App() {
+import './component/Expence';
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h2>Let's get started</h2>
+        <Expence></Expence>
     </div>
+    
   );
 }
-
 export default App;
---
+++ expence_tracker/src/component/Expence.js
@@ -0,0 +1,13 @@
+import 'Expence.css';
+function Expence(){
+    return (
+        <div>
+            <div>March 12th 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$200</div>
+            </div>
+        </div>
+    );
+}
+export default Expence;
