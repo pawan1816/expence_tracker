e4b11ea5a59f6b82ab7096dfec35705e3f06be49
pawan kumar paik <paikpawan18@gmail.com>
2023-12-07 21:22:33 +0530

    ultiple states in react

--- expence_tracker/src/component/NewExpenses/ExpenseForm.js
+++ expence_tracker/src/component/NewExpenses/ExpenseForm.js
@@ -1,26 +1,35 @@
-import React from "react";
+import React,  { useState } from "react";
  
 import './ExpenseForm.css';
 
 const ExpenseForm = ()=>{
+     const [enteredTitle,setEnteredTitle]=useState("");
+     const [enteredAmount,setEnteredAmount]=useState("");
+     const [enteredDate,setEnteredDate]=useState("");
+
+     const titleChangeHandler =(event)=>{
+          console.log(event.target.value);
+     };
+     const amountChangeHandler =(event)=>{};
+     const dateChangeHandler =(event)=>{};
      return(
         <form>
             <div className="new-expense_controls">
                 <div className="new-expense_control">
                      <level>Title</level>
-                     <input type="text"/>
+                     <input type="text" onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense_control">
                      <level>Amounts</level>
-                     <input type="number" min="0.01" step="0.01"/>
+                     <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense_control">
                      <level>Date</level>
-                     <input type="date"/>
+                     <input type="date" onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense_actions">
-                <button type="submit">Add Expense</button>
+               <button type="submit">Add Expense</button>
             </div>
         </form>
      );
