5adab8452e18fa4eb80ae2a98604f51c2c15f415
pawan kumar paik <paikpawan18@gmail.com>
2023-12-07 22:34:34 +0530

    Rendering List in React

--- expence_tracker/src/App.js
+++ expence_tracker/src/App.js
@@ -1,36 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Exp from "./component/Expenses/Exp";
 
 import NewExpense from './component/NewExpenses/NewExpense';
+
+let DummyExpeses =[
+  {
+    id:'e1',
+    title: 'scool fee',
+    amount: 250,
+    date: new Date(2021, 5, 12)
+  },
+  {
+    id:'e2',
+    title: 'books',
+    amount:230,
+    date: new Date(2021, 12, 11)
+  },
+  {
+    id:'e2',
+    title: 'house rant',
+    amount:500,
+    date: new Date(2021, 3, 2)
+  },
+  {
+    id:'e1',
+    title: 'food',
+    amount:530,
+    date: new Date(2023, 2, 2)
+  }
+];
 function App(){
-  let expenses =[
-    {
-      id:'e1',
-      title: 'scool fee',
-      amount: 250,
-      date: new Date(2021, 5, 12)
-    },
-    {
-      id:'e2',
-      title: 'books',
-      amount:230,
-      date: new Date(2021, 12, 11)
-    },
-    {
-      id:'e2',
-      title: 'house rant',
-      amount:500,
-      date: new Date(2021, 3, 2)
-    },
-    {
-      id:'e1',
-      title: 'food',
-      amount:530,
-      date: new Date(2023, 2, 2)
-    }
-  ];
+  const [expenses,setExpenses]=useState(DummyExpeses);
   const addExpenseHandler =(expense)=>{
-    console.log(expense);
+    const updatedExpenses=[expense,...expenses]
+    setExpenses(updatedExpenses);
   };
   return (
     <div>
